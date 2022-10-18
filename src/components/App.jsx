32ec99627e34eca9a135095a21881a7587f559e3
wisang <wseom7@gmail.com>
2022-10-18 17:58:56 +0900

    single responsibility

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import Form from "./Form";
+
+var isLoggedIn = false;
+
+function renderConditionally() {
+
+}
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <div className="container">{
+    isLoggedIn ? <h1>Hello</h1> : <Form />
+    }
+  </div>;
 }
 
 export default App;
